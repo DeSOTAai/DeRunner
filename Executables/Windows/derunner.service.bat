436ceaf3bcfd10f35dd156cfbfeb0873a6814017
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-28 22:01:45 +0100

    Service Log DeRunner prints

--- Executables/Windows/derunner.service.bat
+++ Executables/Windows/derunner.service.bat
@@ -6,6 +6,6 @@ for %%a in ("%~dp0..\..") do set "model_path_serv=%%~fa"
 
 :: Run DeRunner Service
 call cd %model_path_serv%
-call %model_path_serv%\env\python DeRunner.py >NUL 2>NUL
+call %model_path_serv%\env\python DeRunner.py
 
 GOTO mainloop
