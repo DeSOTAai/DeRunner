d5f39cd4feebfd6129541174daa847a1d01afb37
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-29 21:11:01 +0100

    Get Service Status

--- Executables/Windows/DeRunner.run.bat
+++
@@ -1,7 +0,0 @@
-@REM  VARS 
-for %%a in ("%~dp0..\..") do set "model_path_run=%%~fa"
-@REM Go to Project Folder
-call cd %model_path_run%
-@REM Run DeRunner
-call %model_path_run%\env\python DeRunner.py
-PAUSE
