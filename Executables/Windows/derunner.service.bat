9d6a6f3bd1341e5cd39accb3ef0f78c1e03b7980
franciscomvargas <franciscomvargas24@gmail.com>
2023-09-14 08:30:20 +0100

    Exitcode 666

--- Executables/Windows/derunner.service.bat
+++ Executables/Windows/derunner.service.bat
@@ -9,4 +9,11 @@ for %%a in ("%~dp0..\..") do set "model_path_serv=%%~fa"
 call cd %model_path_serv%
 call %model_path_serv%\env\python DeRunner.py
 
+IF errorlevel 666 GOTO exit_mainloop
+
 GOTO mainloop
+
+:exit_mainloop
+ECHO DeRunner Requested EXIT
+start /B /WAIT %model_path_serv%\executables\windows\derunner.stop.bat
+exit
