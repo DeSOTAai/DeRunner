4c46478f17a85fa7b053590d741459beda2c0fc4
franciscomvargas <franciscomvargas24@gmail.com>
2023-10-28 11:40:05 +0100

    Under Dev...

--- Tools/after_model_reinstall.py
+++ Tools/after_model_reinstall.py
@@ -38,100 +38,100 @@ USER_MODELS = USER_CONF['models']
 USER_SYSTEM = USER_CONF['system']
 
 
-IGNORE_MODELS = ["desotaai/derunner"]
+# IGNORE_MODELS = ["desotaai/derunner"]
 
-# retieved from https://stackoverflow.com/a/11995662  && https://stackoverflow.com/a/10052222 && https://stackoverflow.com/a/40388766
-GET_ADMIN = [
-    "net session >NUL 2>NUL\n",
-    "IF %errorLevel% NEQ 0 (\n",
-    "\tgoto UACPrompt\n",
-    ") ELSE (\n",
-    "\tgoto gotAdmin\n",
-    ")\n",
-    ":UACPrompt\n",
-    'powershell -Command "Start-Process -Wait -Verb RunAs -FilePath \'%0\' -ArgumentList \'am_admin\'" \n',
-    "exit /B\n",
-    ":gotAdmin\n",
-    'pushd "%CD%"\n',
-    f'CD /D "{DESOTA_ROOT_PATH}"\n'
-]
+# # retieved from https://stackoverflow.com/a/11995662  && https://stackoverflow.com/a/10052222 && https://stackoverflow.com/a/40388766
+# GET_ADMIN = [
+#     "net session >NUL 2>NUL\n",
+#     "IF %errorLevel% NEQ 0 (\n",
+#     "\tgoto UACPrompt\n",
+#     ") ELSE (\n",
+#     "\tgoto gotAdmin\n",
+#     ")\n",
+#     ":UACPrompt\n",
+#     'powershell -Command "Start-Process -Wait -Verb RunAs -FilePath \'%0\' -ArgumentList \'am_admin\'" \n',
+#     "exit /B\n",
+#     ":gotAdmin\n",
+#     'pushd "%CD%"\n',
+#     f'CD /D "{DESOTA_ROOT_PATH}"\n'
+# ]
 
-#   > Update Models Starter for Run constantly models
-def upd_model_start():
-    if USER_SYSTEM == "win":
-        # 2 - Get Admin Previleges 
-        _tmp_file_lines = [
-            "@ECHO OFF\n"
-        ]
-        _tmp_file_lines += GET_ADMIN
+# #   > Update Models Starter for Run constantly models
+# def upd_model_start():
+#     if USER_SYSTEM == "win":
+#         # 2 - Get Admin Previleges 
+#         _tmp_file_lines = [
+#             "@ECHO OFF\n"
+#         ]
+#         _tmp_file_lines += GET_ADMIN
 
-        _exist_run_constantly_model = False
-        for model_id, model_v in USER_MODELS.items():
+#         _exist_run_constantly_model = False
+#         for model_id, model_v in USER_MODELS.items():
 
-            service_params = LAST_SERV_CONF["services_params"][model_id] if LAST_SERV_CONF["services_params"][model_id][USER_SYSTEM]["version"] == model_v else SERV_CONF["services_params"][model_id]
+#             service_params = LAST_SERV_CONF["services_params"][model_id] if LAST_SERV_CONF["services_params"][model_id][USER_SYSTEM]["version"] == model_v else SERV_CONF["services_params"][model_id]
             
-            _model_param_run_constantly = service_params["run_constantly"]
-            if not _model_param_run_constantly:
-                continue
+#             _model_param_run_constantly = service_params["run_constantly"]
+#             if not _model_param_run_constantly:
+#                 continue
 
-            if not _exist_run_constantly_model:
-                _exist_run_constantly_model = True
+#             if not _exist_run_constantly_model:
+#                 _exist_run_constantly_model = True
 
-            _model_serv_path = service_params[USER_SYSTEM]["service_path"]
-            _model_serv_start = service_params[USER_SYSTEM]["starter"]
-            if _model_serv_start:
-                _model_start_path = os.path.join(USER_PATH, _model_serv_path, _model_serv_start)
+#             _model_serv_path = service_params[USER_SYSTEM]["service_path"]
+#             _model_serv_start = service_params[USER_SYSTEM]["starter"]
+#             if _model_serv_start:
+#                 _model_start_path = os.path.join(USER_PATH, _model_serv_path, _model_serv_start)
                 
-                _tmp_file_lines.append(f"start /B /WAIT {_model_start_path}\n")
+#                 _tmp_file_lines.append(f"start /B /WAIT {_model_start_path}\n")
 
-        if not _exist_run_constantly_model:
-            if os.path.isfile(SERVICES_START_PATH):
-                os.remove(SERVICES_START_PATH)
-            return
+#         if not _exist_run_constantly_model:
+#             if os.path.isfile(SERVICES_START_PATH):
+#                 os.remove(SERVICES_START_PATH)
+#             return
         
-        _tmp_file_lines.append("exit\n")
+#         _tmp_file_lines.append("exit\n")
 
-        # 4 - Create Starter Bat
-        with open(SERVICES_START_PATH, "w") as fw:
-            fw.writelines(_tmp_file_lines)
+#         # 4 - Create Starter Bat
+#         with open(SERVICES_START_PATH, "w") as fw:
+#             fw.writelines(_tmp_file_lines)
             
-        return
+#         return
             
-#   > Update Models Stoper
-def upd_model_stop():
-    if USER_SYSTEM == "win":
-        # 2 - Get Admin Previleges 
-        _tmp_file_lines = [
-            "@ECHO OFF\n"
-        ]
-        _tmp_file_lines += GET_ADMIN
+# #   > Update Models Stoper
+# def upd_model_stop():
+#     if USER_SYSTEM == "win":
+#         # 2 - Get Admin Previleges 
+#         _tmp_file_lines = [
+#             "@ECHO OFF\n"
+#         ]
+#         _tmp_file_lines += GET_ADMIN
         
-        _exist_model2stop = False
-        for model_id, model_v in USER_MODELS.items():
-            service_params = LAST_SERV_CONF["services_params"][model_id] if LAST_SERV_CONF["services_params"][model_id][USER_SYSTEM]["version"] == model_v else SERV_CONF["services_params"][model_id]
+#         _exist_model2stop = False
+#         for model_id, model_v in USER_MODELS.items():
+#             service_params = LAST_SERV_CONF["services_params"][model_id] if LAST_SERV_CONF["services_params"][model_id][USER_SYSTEM]["version"] == model_v else SERV_CONF["services_params"][model_id]
             
-            if not _exist_model2stop:
-                _exist_model2stop = True
+#             if not _exist_model2stop:
+#                 _exist_model2stop = True
 
-            _model_serv_path = service_params[USER_SYSTEM]["service_path"]
-            _model_serv_stop = service_params[USER_SYSTEM]["stoper"]
-            if _model_serv_stop:
-                _model_stop_path = os.path.join(USER_PATH, _model_serv_path, _model_serv_stop)
+#             _model_serv_path = service_params[USER_SYSTEM]["service_path"]
+#             _model_serv_stop = service_params[USER_SYSTEM]["stoper"]
+#             if _model_serv_stop:
+#                 _model_stop_path = os.path.join(USER_PATH, _model_serv_path, _model_serv_stop)
                 
-                _tmp_file_lines.append(f"start /B /WAIT {_model_stop_path}\n")
+#                 _tmp_file_lines.append(f"start /B /WAIT {_model_stop_path}\n")
 
-        if not _exist_model2stop:
-            if os.path.isfile(SERVICES_STOP_PATH):
-                os.remove(SERVICES_STOP_PATH)
-            return
+#         if not _exist_model2stop:
+#             if os.path.isfile(SERVICES_STOP_PATH):
+#                 os.remove(SERVICES_STOP_PATH)
+#             return
         
-        _tmp_file_lines.append("exit\n")
+#         _tmp_file_lines.append("exit\n")
 
-        # 4 - Create Starter Bat
-        with open(SERVICES_STOP_PATH, "w") as fw:
-            fw.writelines(_tmp_file_lines)
+#         # 4 - Create Starter Bat
+#         with open(SERVICES_STOP_PATH, "w") as fw:
+#             fw.writelines(_tmp_file_lines)
 
-        return
+#         return
     
 #   > Update Services Config with params from Latest Services Config
 def upd_serv_conf():
@@ -147,8 +147,8 @@ def upd_serv_conf():
         
 
 def main():
-    upd_model_start()
-    upd_model_stop()
+    # upd_model_start()
+    # upd_model_stop()
     upd_serv_conf()
 
 if __name__ == "__main__":
