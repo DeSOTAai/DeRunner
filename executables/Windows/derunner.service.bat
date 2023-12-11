b7e636b7ea132caff5ba9bd07e2a1b4595842a89
Francisco Vargas <87917356+franciscomvargas@users.noreply.github.com>
2023-12-11 02:04:15 +0000

    Revision: Win Execs

--- executables/Windows/derunner.service.bat
+++ executables/Windows/derunner.service.bat
@@ -1,19 +1,20 @@
 @ECHO OFF
-:mainloop
-
 :: Get Model path
 :: %~dp0 = C:\users\[user]\Desota\DeRunner\executables\Windows
-for %%a in ("%~dp0..\..") do set "model_path_serv=%%~fa"
-
-:: Run DeRunner Service
-call cd %model_path_serv%
-call %model_path_serv%\env\python DeRunner.py
-
+for %%a in ("%~dp0..\..") do set "model_path=%%~fa"
+:: Move to Model Path
+call cd %model_path%
+:: Delete Service Log on-start
+break>%model_path%\service.log
+:: Make sure every package required is installed
+call %model_path%\env\python -m pip install -r %model_path%\requirements.txt
+:mainloop
+:: Start DeRunner Service
+call %model_path%\env\python DeRunner.py
 IF errorlevel 66 GOTO exit_mainloop
-
 GOTO mainloop
 
 :exit_mainloop
 ECHO DeRunner Requested EXIT
-start /B /WAIT %model_path_serv%\executables\windows\derunner.stop.bat
+start /B /WAIT %model_path%\executables\windows\derunner.stop.bat
 exit
